43b662ba06b3fe170181ac12d4394e3e060d56e2
FMD <Info.franklydear@gmail.com>
2026-05-23 17:27:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,3 @@
-// Απλός Service Worker για να ενεργοποιηθεί το beforeinstallprompt
-self.addEventListener('install', function(event) {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', function(event) {
-  event.waitUntil(clients.claim());
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
-});
+self.addEventListener('install', function(event) { self.skipWaiting(); });
+self.addEventListener('activate', function(event) { event.waitUntil(clients.claim()); });
+self.addEventListener('fetch', function(event) { event.respondWith(fetch(event.request)); });
